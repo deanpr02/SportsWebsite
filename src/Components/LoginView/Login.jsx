ee3e99bd782ce332db7a77b8d25743fff884d3ea
Dean Prach <deanprach@gmail.com>
2024-12-07 16:49:54 -0700

    added base visual code for login

--- src/Components/LoginView/Login.jsx
+++ src/Components/LoginView/Login.jsx
@@ -1,4 +1,5 @@
 import "./Login.css"
+import { FaUser,FaLock } from 'react-icons/fa';
 import NewYorkImage from "../../assets/login-resources/newyork/newyork-skyline.jpg"
 import NewYorkStars from "../../assets/login-resources/newyork/stars-ny.png"
 
@@ -17,7 +18,31 @@ export default function Login() {
     return (
       <>
         <BackgroundImage cityImg={randCity.city} starsImg={randCity.stars} cityName={randCity.name}/>
-        <p>Login</p>
+        <div className="login-frame">
+          <form action="">
+            <h1>Welcome!</h1>
+            <div className="input-box">
+              <input type="text" placeholder="Username" required/>
+              <FaUser className="icon"/>
+            </div>
+            <div className="input-box">
+              <input type="password" placeholder="Password" required/>
+              <FaLock className="icon"/>
+            </div>
+
+            <div className="remember-forgot">
+              <label><input type="checkbox"/>Remember me</label>
+              <a href="#">Forgot password?</a>
+            </div>
+
+            <button type="submit">Login</button>
+
+            <div className="register-link">
+              <p>Dont have an account? <a href="#">Register</a></p>
+
+            </div>
+          </form>
+        </div>
       </>
     )
   }
